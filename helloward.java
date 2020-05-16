cac6f9c4ccb481829f42f3659b60ed3e9707b115
mayur navale <mayurnavale96@gmail.com>
2020-05-16 16:28:17 +0530

    require change in java file

--- helloward.java
+++ helloward.java
@@ -4,5 +4,16 @@ public class MyFirstJavaProgram {
 */
 public static void main(String []args) {
 System.out.println("Hello World"); // prints Hello World
+
+}
+public class Test {
+public static void main(String args[]){
+int a, b;
+a = 10;
+b = (a == 1) ? 20: 30;
+System.out.println( "Value of b is : " + b );
+b = (a == 10) ? 20: 30;
+System.out.println( "Value of b is : " + b );
+}
 }
 }
