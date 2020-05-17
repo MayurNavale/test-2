6f0b89dc4ad6d6f2a7e2cac8ff04b27de40189bb
MayurNavale <45039452+MayurNavale@users.noreply.github.com>
2020-05-17 14:14:39 +0530

    add additional feature

--- helloward.java
+++ helloward.java
@@ -12,8 +12,10 @@ int a, b;
 a = 10;
 b = (a == 1) ? 20: 30;
 System.out.println( "Value of b is : " + b );
+  System.out.println("it is and 20"); 
 b = (a == 10) ? 20: 30;
 System.out.println( "Value of b is : " + b );
+  System.out.println(" it is an 30"); 
 }
 }
 }
